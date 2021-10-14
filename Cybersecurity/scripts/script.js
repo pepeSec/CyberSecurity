85537f2bc23a3497b67a219df8336a37c88d692f
Henrique Marques Feliciano <devweb.pepe@gmail.com>
2021-10-14 00:46:44 -0300

    Update

---
+++ Cybersecurity/scripts/script.js
@@ -0,0 +1,10 @@
+function modifica(n) {
+    var nlink1 = document.querySelector('a#nlink1')
+    var nlink2 = document.querySelector('a#nlink2')
+    var nlink3 = document.querySelector('a#nlink3')
+
+    if (n == 1) {
+        nlink1.style.transition = '400ms'
+        nlink1.style.backgroundColor = 'gray'
+    }
+}
