19a97485285cc58e03f5a65f3db8d3a8b3511695
Henrique Marques Feliciano <devweb.pepe@gmail.com>
2021-10-15 18:48:18 -0300

    Update

--- Cybersecurity/scripts/script.js
+++ Cybersecurity/scripts/script.js
@@ -6,23 +6,29 @@ function modifica(n) {
     if (n == 1) {
         nlink1.style.transition = '400ms'
         nlink1.style.backgroundColor = 'gray'
+        nlink1.style.color = 'white'
     } else if (n == 2) {
         nlink2.style.transition = '400ms'
         nlink2.style.backgroundColor = 'gray'
+        nlink2.style.color = 'white'
     } else if (n == 3) {
         nlink3.style.transition = '400ms'
         nlink3.style.backgroundColor = 'gray'
+        nlink3.style.color = 'white'
     }
 }
 function atualiza(n) {
     if (n == 1) {
         nlink1.style.transition = '400ms'
         nlink1.style.backgroundColor = 'transparent'
+        nlink1.style.color = 'black'
     } else if (n == 2) {
         nlink2.style.transition = '400ms'
         nlink2.style.backgroundColor = 'transparent'
+        nlink2.style.color = 'black'
     } else if (n == 3) {
         nlink3.style.transition = '400ms'
         nlink3.style.backgroundColor = 'transparent'
+        nlink3.style.color = 'black'
     }
 }
